3824309b72de3884d6af602e6dfaabf248fa48c6
mayur kumar <kumarmayur797@gmail.com>
2021-10-29 16:15:14 +0530

    change icons

--- src/components/navBar/AddPlaylists.js
+++ src/components/navBar/AddPlaylists.js
@@ -11,7 +11,7 @@ export class AddPlaylists extends Component {
 		createPlaylist(data).then((playlist) => {
 			this.props.addPlaylist(playlist);
 		});
-		this.setState({ input: "",isEditing: false });
+		this.setState({ input: "", isEditing: false });
 	};
 
 	renderTitile = () => {
@@ -29,7 +29,7 @@ export class AddPlaylists extends Component {
 					<path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z"></path>
 				</svg>
 				<p className="font-bold text-gray-800 tracking-wide">
-					Add new playlist
+					Add New playlist
 				</p>
 			</div>
 		);
--- src/containers/Song.js
+++ src/containers/Song.js
@@ -15,14 +15,25 @@ export class Song extends Component {
 		return (
 			<div className="p-1 ">
 				<div className="flex justify-between items-center px-4">
-					<div>
-						<p className="font-semibold text-2xl tracking-wider text-gray-900 ">
-							{song}
-						</p>
-						<p className="text-black-900 italic font-light">- {artist}</p>
-						<p className="text-gray-800 px-2 font-light text-sm font-serif">
-							{year}
-						</p>
+					<div className="flex gap-2 ">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							width="24"
+							height="24"
+						>
+							<path fill="none" d="M0 0h24v24H0z" />
+							<path d="M20 3v14a4 4 0 1 1-2-3.465V6H9v11a4 4 0 1 1-2-3.465V3h13z" />
+						</svg>
+						<div>
+							<p className="font-semibold text-2xl tracking-wider text-gray-900 ">
+								{song}
+							</p>
+							<p className="text-black-900 italic font-light">- {artist}</p>
+							<p className="text-gray-800 px-2 font-light text-sm font-serif">
+								{year}
+							</p>
+						</div>
 					</div>
 					<div>
 						{this.state.openMenu && (
